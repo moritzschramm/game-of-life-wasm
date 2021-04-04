e0010404522a263e887c02f4004def172a034ca8
Moritz Schramm <x@mo5.ch>
2021-04-04 20:40:31 +0200

    implement first version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod utils;
 
+extern crate fixedbitset;
+
 use wasm_bindgen::prelude::*;
+use fixedbitset::FixedBitSet;
+
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +12,130 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, game-of-life-wasm!");
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+
+        let mut count = 0;
+
+        let north = if row == 0 {
+        self.height - 1
+        } else {
+            row - 1
+        };
+
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+
+
+        count
+    }
+
+    pub fn tick(&mut self) {
+
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                next.set(idx, match (cell, live_neighbors) {
+
+                    (true, x) if x < 2 => false,                // living cell, <2 living neighbors
+                    (true, 2) | (true, 3) => true,              // living cell with 2 or 3 living neighbors
+                    (true, x) if x > 3 => false,                // living cell, >3 living neighbors
+                    (false, 3) => true,                         // dead cell with 3 living neighbors
+                    (otherwise, _) => otherwise,
+
+                });
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new() -> Universe {
+
+        let width = 256;
+        let height = 256;
+
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
 }
